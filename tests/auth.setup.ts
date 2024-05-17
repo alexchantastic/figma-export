fbb2240c962c86644f6fe657915d679e3121078c
Alex Chan <alexchantastic@users.noreply.github.com>
2024-05-17 16:44:47 -0700

    clarify figma env variables

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -8,10 +8,10 @@ const authFile = ".auth/user.json";
 setup("authenticate", async ({ page }) => {
   await page.goto("https://www.figma.com/login");
 
-  await page.getByRole("textbox", { name: "email" }).fill(process.env.EMAIL!);
+  await page.getByRole("textbox", { name: "email" }).fill(process.env.FIGMA_EMAIL!);
   await page
     .getByRole("textbox", { name: "password" })
-    .fill(process.env.PASSWORD!);
+    .fill(process.env.FIGMA_PASSWORD!);
 
   await page.getByRole("button", { name: "log in" }).click();
 
