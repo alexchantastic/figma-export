cfa41d592bd678ce086646b3c3ed3cc14cb955f9
Alex Chan <alexchantastic@users.noreply.github.com>
2024-05-23 16:09:34 -0700

    set correct file extension

--- automations/download.spec.ts
+++ automations/download.spec.ts
@@ -21,8 +21,13 @@ for (const project of projects) {
         await page.getByTestId("dropdown-option-Save local copy…").click();
 
         const download = await downloadPromise;
+        const suggestedFilename = download.suggestedFilename();
+        const filename =
+          suggestedFilename.slice(0, -4) +
+          ` (${file.key}).` +
+          suggestedFilename.slice(-3);
         await download.saveAs(
-          `${process.env.DOWNLOAD_PATH!}${project.name} (${project.id})/${file.name} (${file.key}).fig`,
+          `${process.env.DOWNLOAD_PATH!}${project.name} (${project.id})/${filename}`,
         );
 
         await page.waitForTimeout(Number(process.env.WAIT_TIMEOUT) || 0);
